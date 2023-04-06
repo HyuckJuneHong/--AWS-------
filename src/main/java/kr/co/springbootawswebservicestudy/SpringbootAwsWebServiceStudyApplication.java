cf1116905d9e2040aae3fea4c4a75b85a934d760
홍혁준 <jjj8337721@naver.com>
2023-04-06 22:56:08 +0900

    second-commit

---
+++ src/main/java/kr/co/springbootawswebservicestudy/SpringbootAwsWebServiceStudyApplication.java
@@ -0,0 +1,13 @@
+package kr.co.springbootawswebservicestudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAwsWebServiceStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootAwsWebServiceStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/springbootawswebservicestudy/SpringbootAwsWebServiceStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.springbootawswebservicestudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAwsWebServiceStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
